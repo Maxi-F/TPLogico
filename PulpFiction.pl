aedf42facf779906904382663664bde6ba85d8d4
Mariano Díaz <37786555+mdiazpereyra@users.noreply.github.com>
2018-07-01 01:12:09 -0300

    Update PulpFiction.pl

--- PulpFiction.pl
+++ PulpFiction.pl
@@ -1,6 +1,8 @@
-%pareja(Persona, Persona)
+%-----BASE DE CONOCIMIENTO-----
+
+%pareja(UnaPersona, OtraPersona)
 pareja(marsellus, mia).
-pareja(pumkin,    honeyBunny).
+pareja(pumkin, honeyBunny).
 pareja(bernardo, bianca).
 pareja(bernardo, charo).
 
@@ -13,111 +15,97 @@ trabajaPara(winston, bernardo).
 trabajaPara(bianca, george).
 trabajaPara(charo, george).
 
-% Informaci ó n base
-% personaje(Nombre, Ocupacion)
-personaje ( pumkin , ladron ([ estacionesDeServicio, licorerias ])).
-personaje ( honeyBunny , ladron ([ licorerias , estacionesDeServicio ])).
-personaje ( vincent , mafioso ( maton )).
-personaje ( jules , mafioso ( maton )).
-personaje ( marsellus , mafioso ( capo )).
-personaje ( winston , mafioso ( resuelveProblemas )).
-personaje ( mia , actriz ([ foxForceFive ])).
-personaje ( butch , boxeador ).
-personaje ( bernardo , mafioso ( cerebro )).
-personaje ( bianca , actriz ([ elPadrino1 ])).
-personaje ( elVendedor , vender ([ humo, iphone ])).
-personaje ( jimmie, vender ([ auto] )).
+%personaje(Nombre, Ocupacion)
+personaje(pumkin, ladron([estacionesDeServicio, licorerias])).
+personaje(honeyBunny, ladron([licorerias, estacionesDeServicio])).
+personaje(vincent, mafioso(maton)).
+personaje(jules, mafioso(maton)).
+personaje(marsellus, mafioso(capo)).
+personaje(winston, mafioso(resuelveProblemas)).
+personaje(mia, actriz([foxForceFive])).
+personaje(butch, boxeador).
+personaje(bernardo, mafioso(cerebro)).
+personaje(bianca, actriz([elPadrino1])).
+personaje(elVendedor, vender([humo, iphone])).
+personaje(jimmie, vender([auto])).
 
-% encargo ( Solicitante , Encargado , Tarea ).
-% las tareas pueden ser cuidar ( Protegido ), ayudar ( Ayudado ),
-buscar ( Buscado , Lugar)
-encargo ( marsellus , vincent , cuidar ( mia )).
-encargo ( vincent , elVendedor , cuidar ( mia )).
-encargo ( marsellus , winston , ayudar ( jules )).
-encargo ( marsellus , winston , ayudar ( vincent )).
-encargo ( marsellus , vincent , buscar ( butch , losAngeles )).
-encargo ( bernardo , vincent , buscar ( jules , fuerteApache )).
-encargo ( bernardo , winston , buscar ( jules , sanMartin )).
-encargo ( bernardo , winston , buscar ( jules , lugano )).
+%encargo(Solicitante, Encargado, Tarea).
+%las tareas pueden ser cuidar(Protegido), ayudar(Ayudado), buscar(Buscado, Lugar)
+encargo(marsellus, vincent, cuidar(mia)).
+encargo(vincent, elVendedor, cuidar(mia)).
+encargo(marsellus, winston, ayudar(jules)).
+encargo(marsellus, winston, ayudar(vincent)).
+encargo(marsellus, vincent, buscar(butch, losAngeles)).
+encargo(bernardo, vincent, buscar(jules, fuerteApache)).
+encargo(bernardo, winston, buscar(jules, sanMartin)).
+encargo(bernardo, winston, buscar(jules, lugano)).
 
-amigo ( vincent , jules ).
-amigo ( jules , jimmie ).
-amigo ( vincent , elVendedor ).
+%amigo(UnaPersona, OtraPersona)
+amigo(vincent, jules).
+amigo(jules, jimmie).
+amigo(vincent, elVendedor).
 
+%----------PARTE 1----------
 
-saleCon(Quien, Con) :-
+saleCon(Quien, Con):-
 	pareja(Quien, Con).
-saleCon(Quien, Con) :-
+saleCon(Quien, Con):-
 	pareja(Con, Quien).
-	
 
-esFiel(Personaje) :-
+esFiel(Personaje):-
 	saleCon(Personaje, _),
 	findall(Pareja, saleCon(Pareja, Personaje), Parejas),
 	length(Parejas, 1).
 
-%CasoBase	
-acataOrden(Empleador, Empleado) :-
+%CasoBase
+acataOrden(Empleador, Empleado):-
 	trabajaPara(Empleador, Empleado).
 %CasoRecursivo
-acataOrden(Empleador, Empleado) :-
+acataOrden(Empleador, Empleado):-
 	trabajaPara(Superior, Empleado),
 	acataOrden(Empleador, Superior).
-	
-	
-%Parte 2
 
-%Punto 1. Personajes peligrosos
+
+%----------PARTE 2----------
 
 esPeligroso(Personaje):-
 	tieneJefePeligroso(Personaje).
-
 esPeligroso(Personaje):-
 	personaje(Personaje, mafioso(maton)).
-
 esPeligroso(Personaje):-
 	personaje(Personaje, ladron(LugaresRobados)),
 	member(licorerias, LugaresRobados).
-
 tieneJefePeligroso(Personaje):-
 	trabajaPara(Jefe, Personaje),
 	esPeligroso(Jefe).
-	
-%Punto 2. San Cayetano
 
-tieneCerca(Persona, Otro):-
-	amigo(Persona, Otro).
-tieneCerca(Persona, Otro):-
-	amigo(Otro, Persona).
-tieneCerca(Persona, Otro):-
-	trabajaPara(Persona, Otro).
-tieneCerca(Persona, Otro):-
-	trabajaPara(Otro, Persona).
+tieneCerca(Persona, OtraPersona):-
+	amigo(Persona, OtraPersona).
+tieneCerca(Persona, OtraPersona):-
+	amigo(OtraPersona, Persona).
+tieneCerca(Persona, OtraPersona):-
+	trabajaPara(Persona, OtraPersona).
+tieneCerca(Persona, OtraPersona):-
+	trabajaPara(OtraPersona, Persona).
 
 sanCayetano(Persona):-
 	tieneCerca(Persona, _),
-	forall(tieneCerca(Persona, Otro), encargo(Persona, Otro, _)).
-	
-%Punto 3. Nivel de Respeto
-
-nivelRespeto(Personaje, 10):-
-	personaje(Personaje, mafioso(resuelveProblemas)).
-
-nivelRespeto(Personaje, 20):-
-	personaje(Personaje, mafioso(capo)).
-
-nivelRespeto(vincent, 15).
+	forall(tieneCerca(Persona, OtraPersona), encargo(Persona, OtraPersona, _)).
 
 nivelRespeto(Personaje, Nivel):-
 	personaje(Personaje, actriz(Peliculas)),
 	length(Peliculas, Cantidad),
 	Nivel is (Cantidad/10).
-
-%Punto 4. Personajes respetables
+nivelRespeto(Personaje, 10):-
+	personaje(Personaje, mafioso(resuelveProblemas)).
+nivelRespeto(Personaje, 20):-
+	personaje(Personaje, mafioso(capo)).
+nivelRespeto(vincent, 15).
 
 respetable(Personaje):-
 	nivelRespeto(Personaje, Nivel),
 	Nivel>9.
+
 noRespetable(Personaje):-
 	nivelRespeto(Personaje, Nivel),
 	Nivel<9.
@@ -138,11 +126,17 @@ respetabilidad(Respetables, NoRespetables):-
 	length(ListaRespetables, Respetables),
 	findall(Personaje, noRespetable(Personaje), ListaNoRespetables),
 	length(ListaNoRespetables, NoRespetables).
+	
+cantidadEncargos(Personaje, CantidadEncargos):-
+	personaje(Personaje, _),
+	findall(Encargo, encargo(_, Personaje, Encargo), ListaEncargos),
+	length(ListaEncargos, CantidadEncargos).
+	
+tieneMasCantidadDeEncargosQue(Personaje, OtroPersonaje):-
+	cantidadEncargos(Personaje, UnaCantidadEncargos),
+	cantidadEncargos(OtroPersonaje, OtraCantidadEncargos),
+	UnaCantidadEncargos >= OtraCantidadEncargos.
 
-%Punto 5. Más atareado
-
-
-					
-					
-
-
+masAtareado(Personaje):-
+	personaje(Personaje, _),
+	forall(personaje(OtroPersonaje, _), tieneMasCantidadDeEncargosQue(Personaje, OtroPersonaje)).
